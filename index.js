428a375b30087afcb61f78dfa3da240c79849135
gnoyeS <sedragon.kim@gmail.com>
2026-01-27 10:35:53 +0900

    feat: Add npm package structure (package.json, index.js)

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * gnoyes-skills
+ * Git/PR 유틸리티 스킬 플러그인
+ */
+
+// 플러그인 메타데이터 export
+export const pluginInfo = {
+  name: 'gnoyes',
+  version: '1.0.2',
+  description: 'Git/PR 유틸리티 스킬 - Smart Commit, Create PR, Review PR'
+};
